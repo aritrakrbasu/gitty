077101b8461cc8c0b7e4456ddd344544fcf23021
Aritra Basu <43692750+aritrakrbasu@users.noreply.github.com>
2021-04-26 23:45:04 +0530

    Updates

---
+++ precache-manifest.7cacc34f47c32c7a33456b34cbceec06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37daabc9d229d234fd2d7753aead4a72",
+    "url": "/gitty/index.html"
+  },
+  {
+    "revision": "cf95d9a5bb1dae9aec4c",
+    "url": "/gitty/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b01c2e9a550999818929",
+    "url": "/gitty/static/css/main.fc5a1251.chunk.css"
+  },
+  {
+    "revision": "cf95d9a5bb1dae9aec4c",
+    "url": "/gitty/static/js/2.38964748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gitty/static/js/2.38964748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01c2e9a550999818929",
+    "url": "/gitty/static/js/main.93622d35.chunk.js"
+  },
+  {
+    "revision": "09beb437f7319ce54dcb",
+    "url": "/gitty/static/js/runtime-main.e0e8c1cd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/gitty/static/media/logo.ee7cd8ed.svg"
+  }
+]);
